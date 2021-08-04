bd1b2b7c30a055d69b77bda7465c017ef4a511bb
aniketsr <aransing2019@gmail.com>
2021-08-04 19:43:11 +0530

    Gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,8 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Hi there! I am Aniket Ransing, creator of this site, which I built during internship in Eacad using Gatsby.
+        I have just completed 2nd year of B. Tech in Computer Science from IIIT Nagpur.</p>
     </Layout>
   )
 }
